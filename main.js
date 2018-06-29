58bb7713710e8ab5bc3964c4ae366da106a591e6
Hov Petrosyan <hovhannes.petrosyan95@gmail.com>
2018-06-29 19:36:44 +0400

    sshabck

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 //console.log(faker.name.findName())
+const fist_img = 'https://thumbs.dreamstime.com/b/fist-hand-cartoon-character-art-illustration-61138595.jpg';
+
 const img_gl = ['https://www.graphicsfactory.com/clip-art/image_files/image/2/1440432-gladiator_cartoon.jpg','https://capetowncaricaturist.files.wordpress.com/2013/08/russell-crowe.jpg','https://thumbs.dreamstime.com/z/cartoon-gladiator-flexing-illustration-muscular-51211992.jpg','https://thumbs.dreamstime.com/b/cartoon-gladiator-18593886.jpg'];
 class Cessar{
   kill(gladiator){
@@ -34,46 +36,15 @@ class Gladiator{
         }
         gladiators[opp_nmb].health-=fake_this.power;gladiators[opp_nmb].health = (gladiators[opp_nmb].health).toFixed(3);/*console.log(gladiators);console.log(gladiators[opp_nmb].health);*/fake_this.ready_for_hit = true;
         console.log(' ['+fake_this.name+' x '+ fake_this.health +'] '+' hits '+' ['+gladiators[opp_nmb].name+' x '+ gladiators[opp_nmb].health+']'+'  with power ' + fake_this.power );
-        let arena = document.getElementById('arena');
-
-        let td1 = document.createElement('td');
-        const imageNode = document.createElement('img');
-        imageNode.src = fake_this.img;
-        imageNode.setAttribute('height',50); 
-        td1.appendChild(imageNode);
+     let arena = document.getElementById('arena');
         let tr = document.createElement('tr');
-        tr.appendChild(td1);
-
-        let td3 = document.createElement('td');
-        const nameNode = document.createTextNode(fake_this.name);
-        td3.appendChild(nameNode);
-        tr.appendChild(td3);
-        arena.appendChild(tr);
-
-        let td2 = document.createElement('td');
-        const healthNode = document.createTextNode(fake_this.health);
-        td2.appendChild(healthNode);
-        tr.appendChild(td2);
-        arena.appendChild(tr);
-
-        let td4 = document.createElement('td');
-        const imageNode1 = document.createElement('img');
-        imageNode1.src = gladiators[opp_nmb].img;
-        imageNode1.setAttribute('height',50); 
-        td4.appendChild(imageNode1);
-        
-        tr.appendChild(td4);
-
-        let td5 = document.createElement('td');
-        const nameNode1 = document.createTextNode(gladiators[opp_nmb].name);
-        td5.appendChild(nameNode1);
-        tr.appendChild(td5);
-        arena.appendChild(tr);
-
-        let td6 = document.createElement('td');
-        const healthNode1 = document.createTextNode(gladiators[opp_nmb].health);
-        td6.appendChild(healthNode1);
-        tr.appendChild(td6);
+        // here goes DOM manipulation ,,,every kick is recorded here
+        imgCreator(fake_this.img,arena,tr);
+        tdCreator(fake_this.name,arena,tr);
+        tdCreator(fake_this.power,arena,tr);
+        imgCreator(fist_img,arena,tr);
+        imgCreator(gladiators[opp_nmb].img,arena,tr);
+        tdCreator(gladiators[opp_nmb].name,arena,tr);
         arena.appendChild(tr);
 
         //console.log(fake_this.timer);
@@ -100,20 +71,31 @@ var tm = setInterval(function(){
       }
       if(gladiators[i].health <= 0 )
       {
+
         console.log('what to do great Cessar?save him or kill? :)');
         console.log('Cessar:hmmmmmmm');
         console.log('Cessar:hmmmmm');
-        if(Math.random()>0.5){
+        if(Math.random()>0.8){
           cessar.kill(gladiators[i]);
           console.log('---------------->');
           console.log(gladiators[i].name + ' has been died');
           console.log('---------------->');
+          let textNd = document.createTextNode('KILL! ' + gladiators[i].name);
+          let cessarNode = document.getElementById('cessar_will');
+          cessarNode.appendChild(textNd);
+          setTimeout(cessarNode.removeChild(textNd),1000);
+          
         }
         else{
           cessar.save(gladiators[i]);
           console.log('---------------->');
           console.log(gladiators[i].name + ' has been saved by Great Cessar');
           console.log('---------------->');
+          let textNd = document.createTextNode('SAVE! ' + gladiators[i].name);
+          let cessarNode = document.getElementById('cessar_will');
+          cessarNode.appendChild(textNd);
+          setTimeout(function(){cessarNode.removeChild(textNd)},1000);
+          
         }
         if(gladiators[i].health < 0){
           clearTimeout(gladiators[i].timer);
@@ -135,5 +117,19 @@ var tm = setInterval(function(){
 },20);
 function stopF(){
   clearInterval(tm);
+}
+function tdCreator(name,arena,tr){
+        let td = document.createElement('td');
+        const nameNode = document.createTextNode(name);
+        td.appendChild(nameNode);
+        tr.appendChild(td);
+}
+function imgCreator(src,arena,tr){
+        let td = document.createElement('td');
+        const imageNode = document.createElement('img');
+        imageNode.src = src;
+        imageNode.setAttribute('height',50); 
+        td.appendChild(imageNode);
+        tr.appendChild(td);
 }
 
